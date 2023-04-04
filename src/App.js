c91f452a0e25422c53960cac173ff282b11cb81e
muXtract <56767342+muXtract@users.noreply.github.com>
2023-04-04 14:49:48 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react"
+import {useEffect} from 'react'
+import { useState } from "react"
+import searchIcon from "./search.svg"
+import Movie1card from "./Movie1Card"
+
+
+
+//90e23b78
+const API_URL='http://www.omdbapi.com/?i=tt3896198&apikey=90e23b78' //stores data from api 
+
+
+const App = () => {
+  const [movies,setMovies]=useState([]);
+  const[searchTerm,setSearchTerm]=useState("");
+
+  const searchMovies=async(title)=>{
+    const response=await fetch(`${API_URL}&s=${title}`)   //fetch api call (returns a promise awaits for the response )
+    const data=await response.json();  //converts in json readable form
+    setMovies(data.Search);
+    
+  }
+  useEffect(()=>{
+    searchMovies('Batman');
+  },[]);
+  return (
+    <div className="app">
+        <h1>NetFlex</h1>
+        <div className="search">
+          <input placeholder="search for movies" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} />
+          <img src={searchIcon} onClick={()=>searchMovies(searchTerm)} />
+        </div>
+      {
+         movies?.length>0
+        ? (<div className="container">
+          {movies.map((movie)=>(
+            <Movie1card movie={movie}/>
+          ))}
+          
+        </div>
+         ):
+         <div className="empty">
+          <h2>API fault</h2>
+         </div>
+      }
+
+
+       
+        
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/Movie1Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Movie1Card = ({movie}) => {
+  return (
+    <div className="movie">
+              <div>
+                <p>{movie.Year}</p>
+              </div>
+              <div >
+                <img src={movie.Poster!=='N/A'? movie.Poster:"https://via.placeholder.com/400" } />
+              </div>
+              <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Poster!=="N/A"?movie.Title=movie.Title:movie.Title="API Fault"}</h3>
+              </div>
+     </div>
+  )
+}
+
+export default Movie1Card;
